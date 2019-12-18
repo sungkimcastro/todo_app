f5871b15999d92c67f2b0afb3bcdc33592aae888
sungkimcastro <35593158+sungkimcastro@users.noreply.github.com>
2019-12-18 15:09:22 +0200

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -27,20 +27,3 @@ module.exports = {
   devtool: "source-map"
 };
 
-// Now webpack manage babel
-
-// Absolute path
-// C:\Users\sj916\Desktop\Sung\javascript\boilerplate\public\scripts
-
-// Node js global variable
-// __dirname: Provides the absolute path to the root of the project
-// // C:\Users\sj916\Desktop\Sung\javascript\boilerplate\
-
-// const path = require("path")
-// require("path"): its a node jslibrary
-// const path: its just an object
-// path.resolve(): its a method that allow us to combine the absolute path
-
-// devServer:
-// contentBase: Takes an absolute path, and serve the folder
-// publicPath: Here we tell to the server where to compile our modules
